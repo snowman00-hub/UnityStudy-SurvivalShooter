297f86ce41ab9416084e3c51cf700eca6c7d1570
윤준모 <ojm01088021296@gmail.com>
2025-08-29 11:40:57 +0900

    이동 추가

---
+++ SurvivalShooter-Practice/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static readonly string AxisHorizontal = "Horizontal";
+    public static readonly string AxisVertical = "Vertical";
+
+    public float HorizontalMove { get; private set;}
+    public float VerticalMove { get; private set;}
+
+    private void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw(AxisHorizontal);
+        VerticalMove = Input.GetAxisRaw(AxisVertical);
+    }
+}
---
+++ SurvivalShooter-Practice/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static readonly int hashMove = Animator.StringToHash("IsMove");
+
+    public float speed = 5.0f;
+
+    private Rigidbody rb;
+    private PlayerInput input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        float h = input.HorizontalMove;
+        float v = input.VerticalMove;
+
+        Vector3 moveDir = (camForward * v + camRight * h).normalized;
+        rb.linearVelocity = moveDir * speed;
+
+        bool isMove = rb.linearVelocity.magnitude > 0;
+        animator.SetBool(hashMove, isMove);
+    }
+}
