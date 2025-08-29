8cabbdef5752d29ee241fb47e2c3bec70ddc6f9c
윤준모 <ojm01088021296@gmail.com>
2025-08-29 15:05:36 +0900

    좀비 추격 기능 추가

---
+++ SurvivalShooter-Practice/Assets/Scripts/Gun.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireDistance = 7f;
+
+    public ParticleSystem shotEffect;
+
+    public Transform firePosition;
+
+    private LineRenderer lineRenderer;
+    private float LastShootTime;
+    public float shootInterval = 0.1f;
+    public float effectWaitTime = 0.1f;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && LastShootTime + shootInterval < Time.time)
+        {
+            LastShootTime = Time.time;
+            Shoot();
+        }   
+    }
+
+    public void Shoot()
+    {
+        Vector3 hitPosition = Vector3.zero;
+
+        RaycastHit hit;
+        if (Physics.Raycast(firePosition.position, firePosition.forward,
+            out hit, fireDistance))
+        {
+            hitPosition = hit.point;
+        }
+        else
+        {
+            hitPosition = firePosition.position + firePosition.forward * fireDistance;
+        }
+
+        StartCoroutine(CoShotEffect(hitPosition));
+    }
+
+    private IEnumerator CoShotEffect(Vector3 hitPosition)
+    {
+        shotEffect.Play();
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, firePosition.position);
+        lineRenderer.SetPosition(1, hitPosition);
+
+        yield return new WaitForSeconds(effectWaitTime);
+
+        lineRenderer.enabled = false;
+    }
+}
---
+++ SurvivalShooter-Practice/Assets/Scripts/Zombie.cs
@@ -0,0 +1,140 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Audio;
+
+public class Zombie : MonoBehaviour
+{
+    private static readonly int hashHasTarget = Animator.StringToHash("HasTarget");
+    private static readonly int hashDie = Animator.StringToHash("Die");
+
+    public enum Status
+    {        
+        Idle,
+        Trace,
+        Attack,
+        Die
+    }
+
+    private Status currentStatus;
+
+    public Status CurrentStatus
+    {
+        get { return currentStatus; }
+        set
+        {
+            var prevStatus = currentStatus;
+            currentStatus = value;
+
+            switch (currentStatus)
+            {
+                case Status.Idle:
+                    animator.SetBool(hashHasTarget, false);
+                    agent.isStopped = true;
+                    break;
+                case Status.Trace:
+                    animator.SetBool(hashHasTarget, true);
+                    agent.isStopped = false;
+                    break;
+                case Status.Attack:
+                    animator.SetBool(hashHasTarget, false);
+                    agent.isStopped = true;
+                    break;
+                case Status.Die:
+                    animator.SetTrigger(hashDie);
+                    agent.isStopped = true;
+                    capsuleCollider.enabled = false;
+                    break;
+            }
+        }
+    }
+
+    public float traceDistance = 10f;
+    public float attackDistance = 0.3f;
+    public LayerMask targetLayer;
+
+    private Transform target;
+    private Animator animator;
+    private NavMeshAgent agent;
+    private CapsuleCollider capsuleCollider;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void Update()
+    {
+        switch (currentStatus)
+        {
+            case Status.Idle:
+                UpdateIdle();
+                break;
+            case Status.Trace:
+                UpdateTrace();
+                break;
+            case Status.Attack:
+                UpdateAttack();
+                break;
+            case Status.Die:
+                UpdateDie();
+                break;
+        }
+    }
+
+    private void UpdateIdle()
+    {
+        if (target != null &&
+            Vector3.Distance(transform.position, target.position) < traceDistance)
+        {
+            CurrentStatus = Status.Trace;
+        }
+
+        target = FindTarget(traceDistance);
+    }
+
+    private void UpdateTrace()
+    {
+        if (target != null &&
+            Vector3.Distance(transform.position, target.position) < attackDistance)
+        {
+            CurrentStatus = Status.Attack;
+            return;
+        }
+
+        if (target == null || 
+            Vector3.Distance(transform.position, target.position) > traceDistance)
+        {
+            CurrentStatus = Status.Idle;
+            return;
+        }
+
+        agent.SetDestination(target.position);
+    }
+
+    private void UpdateAttack()
+    {
+        CurrentStatus = Status.Idle; // 테스트코드
+
+    }
+
+    private void UpdateDie()
+    {
+
+    }
+
+    private Transform FindTarget(float radius)
+    {
+        var colliders = Physics.OverlapSphere(transform.position, radius, targetLayer.value);
+        if (colliders.Length == 0)
+        {
+            return null;
+        }
+
+        var target = colliders.OrderBy(
+           x => Vector3.Distance(x.transform.position, transform.position)).First();
+        return target.transform;
+    }
+}
